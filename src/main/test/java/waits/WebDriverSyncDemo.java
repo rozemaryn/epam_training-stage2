3c8fab475d08c6417d1dcf763ff45fc855e8e75a
Maria Kostyanetskaya <animalitos@yandex.ru>
2023-10-04 10:49:07 +0300

    examples of general timeouts

--- src/main/test/java/waits/WebDriverSyncDemo.java
+++ src/main/test/java/waits/WebDriverSyncDemo.java
@@ -1,16 +1,22 @@
+package waits;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
-public class HelloWebDriver {
-    public static void main(String[] args) throws InterruptedException {
+public class WebDriverSyncDemo {
+    public static void main(String[] args) {
         WebDriver driver = new ChromeDriver();
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(15));
+        driver.manage().timeouts().scriptTimeout(Duration.ofSeconds(10));
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+
         driver.get("https://selenium.dev");
         WebElement searchInput = driver.findElement(By.xpath(
                 "//span[@class='DocSearch-Button-Placeholder']"));
@@ -20,9 +26,6 @@ public class HelloWebDriver {
         searchPanel.get(0).sendKeys("java selenium");
         searchPanel.get(0).sendKeys(Keys.ENTER);
 
-        Thread.sleep(2000);
         driver.quit();
-
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(4));
     }
 }
