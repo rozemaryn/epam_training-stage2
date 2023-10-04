b63698b4f67e05b49e312e6416f6107069ef4f2f
Maria Kostyanetskaya <animalitos@yandex.ru>
2023-10-04 11:38:49 +0300

    Explicit Wait example

--- src/main/test/java/waits/WebDriverSyncDemo.java
+++ src/main/test/java/waits/WebDriverSyncDemo.java
@@ -5,20 +5,22 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
 import java.util.List;
-import java.util.concurrent.TimeUnit;
 
 public class WebDriverSyncDemo {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         WebDriver driver = new ChromeDriver();
         driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(15));
         driver.manage().timeouts().scriptTimeout(Duration.ofSeconds(10));
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
 
         driver.get("https://selenium.dev");
-        WebElement searchInput = driver.findElement(By.xpath(
+
+        WebElement searchInput = waitForElementLocatedBy(driver, By.xpath(
                 "//span[@class='DocSearch-Button-Placeholder']"));
         searchInput.click();
 
@@ -26,6 +28,13 @@ public class WebDriverSyncDemo {
         searchPanel.get(0).sendKeys("java selenium");
         searchPanel.get(0).sendKeys(Keys.ENTER);
 
+        Thread.sleep(10000);
+
         driver.quit();
+    }
+
+    private static WebElement waitForElementLocatedBy(WebDriver driver, By by) {
+        return new WebDriverWait(driver, Duration.ofSeconds(10)).
+                until(ExpectedConditions.presenceOfElementLocated(by));
     }
 }
